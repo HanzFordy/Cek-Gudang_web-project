fb823205635e0617180aba28c9be757282220365
akmal <makmalthoriq31@gmail.com>
2023-06-27 10:06:47 +0700

    revisi logo

--- resources/views/barang.blade.php
+++ resources/views/barang.blade.php
@@ -119,7 +119,9 @@
                 
                     <div class="col">
                         <div class="bg-light rounded d-flex align-items-center p-4">
-                            <i class="fa fa-chart-line fa-3x text-primary"></i>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-box-seam-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003 6.97 2.789ZM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461L10.404 2Z"/>
+</svg>
                             <div class="ms-3">
                                 <p class="mb-2">Total Barang</p>
                                 <h6 class="mb-0">{{ $banyakBarang }}</h6>
